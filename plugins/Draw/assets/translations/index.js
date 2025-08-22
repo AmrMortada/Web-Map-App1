41f00353fe9cac8c8a346f11026e63debe25bfdc
AmrMortada <165253026+AmrMortada@users.noreply.github.com>
2025-08-22 18:05:15 +0300

    Add files via upload

---
+++ plugins/Draw/assets/translations/index.js
@@ -0,0 +1,67 @@
+import en from './en.json';
+import de from './de.json';
+import it from './it.json';
+import id from './id.json';
+import ro from './ro.json';
+import ru from './ru.json';
+import es from './es.json';
+import nl from './nl.json';
+import fr from './fr.json';
+import zh from './zh.json';
+// eslint-disable-next-line camelcase
+import zh_tw from './zh_tw.json';
+// eslint-disable-next-line camelcase
+import pt_br from './pt_br.json';
+// eslint-disable-next-line camelcase
+import pt_pt from './pt_pt.json';
+import pl from './pl.json';
+import sv from './sv.json';
+import el from './el.json';
+import hu from './hu.json';
+import da from './da.json';
+import no from './no.json';
+import fa from './fa.json';
+import ua from './ua.json';
+import tr from './tr.json';
+import cz from './cz.json';
+import ja from './ja.json';
+import fi from './fi.json';
+import ko from './ko.json';
+import ky from './ky.json';
+
+// eslint-disable-next-line camelcase
+const pt = pt_pt;
+
+export default {
+  en,
+  de,
+  it,
+  id,
+  ro,
+  ru,
+  es,
+  nl,
+  fr,
+  pt,
+  // eslint-disable-next-line camelcase
+  pt_br,
+  // eslint-disable-next-line camelcase
+  pt_pt,
+  zh,
+  // eslint-disable-next-line camelcase
+  zh_tw,
+  pl,
+  sv,
+  el,
+  hu,
+  da,
+  no,
+  fa,
+  ua,
+  tr,
+  cz,
+  ja,
+  fi,
+  ko,
+  ky,
+};
---
+++ plugins/Draw/js/Mixins/Keyboard.js
@@ -0,0 +1,57 @@
+// use function to create a new mixin object for keeping isolation
+// to make it work for multiple map instances
+const createKeyboardMixins = () => ({
+  _lastEvents: { keydown: undefined, keyup: undefined, current: undefined },
+  _initKeyListener(map) {
+    this.map = map;
+    L.DomEvent.on(document, 'keydown keyup', this._onKeyListener, this);
+    L.DomEvent.on(window, 'blur', this._onBlur, this);
+    // clean up global listeners when current map instance is destroyed
+    map.once('unload', this._unbindKeyListenerEvents, this);
+  },
+  _unbindKeyListenerEvents() {
+    L.DomEvent.off(document, 'keydown keyup', this._onKeyListener, this);
+    L.DomEvent.off(window, 'blur', this._onBlur, this);
+  },
+  _onKeyListener(e) {
+    let focusOn = 'document';
+
+    // .contains only supported since IE9, if you want to use Geoman with IE8 or lower you need to implement a polyfill for .contains
+    // with focusOn the user can add a check if the key was pressed while the user interacts with the map
+    if (this.map.getContainer().contains(e.target)) {
+      focusOn = 'map';
+    }
+
+    const data = { event: e, eventType: e.type, focusOn };
+    this._lastEvents[e.type] = data;
+    this._lastEvents.current = data;
+
+    this.map.pm._fireKeyeventEvent(e, e.type, focusOn);
+  },
+  _onBlur(e) {
+    e.altKey = false;
+    const data = { event: e, eventType: e.type, focusOn: 'document' };
+    this._lastEvents[e.type] = data;
+    this._lastEvents.current = data;
+  },
+  getLastKeyEvent(type = 'current') {
+    return this._lastEvents[type];
+  },
+  isShiftKeyPressed() {
+    return this._lastEvents.current?.event.shiftKey;
+  },
+  isAltKeyPressed() {
+    return this._lastEvents.current?.event.altKey;
+  },
+  isCtrlKeyPressed() {
+    return this._lastEvents.current?.event.ctrlKey;
+  },
+  isMetaKeyPressed() {
+    return this._lastEvents.current?.event.metaKey;
+  },
+  getPressedKey() {
+    return this._lastEvents.current?.event.key;
+  },
+});
+
+export default createKeyboardMixins;
